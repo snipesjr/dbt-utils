e1f91e26615ed629b41a553c11607cbd18f3eb7d
Jake Snipes <jakesnipes@outlook.com>
2023-09-07 12:11:33 -0400

    synapse doesn't support ilike so remove it

--- macros/sql/get_tables_by_pattern_sql.sql
+++ macros/sql/get_tables_by_pattern_sql.sql
@@ -10,9 +10,9 @@
             table_name as {{ adapter.quote('table_name') }},
             {{ dbt_utils.get_table_types_sql() }}
         from {{ database }}.information_schema.tables
-        where table_schema ilike '{{ schema_pattern }}'
-        and table_name ilike '{{ table_pattern }}'
-        and table_name not ilike '{{ exclude }}'
+        where table_schema like '{{ schema_pattern }}'
+        and table_name like '{{ table_pattern }}'
+        and table_name not like '{{ exclude }}'
 
 {% endmacro %}
 
